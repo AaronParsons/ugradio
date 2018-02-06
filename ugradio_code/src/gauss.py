f9556bcc393876256e53adb67cc81bb0d9a595dd
Aaron Parsons <aparsons@berkeley.edu>
2018-02-06 10:13:58 -0800

    Added capability to fit multiple gaussians to ugradio.gauss

--- ugradio_code/src/gauss.py
+++ ugradio_code/src/gauss.py
@@ -1,29 +1,45 @@
 import numpy as np
 from scipy.optimize import curve_fit
 
+def _pack_prms(amp, avg, sig):
+    '''Internal. Convert user arguments to parameter list.'''
+    prms = np.array([amp, avg, sig])
+    prms.shape = (3,-1)
+    return prms.T.flatten()
+
+def _unpack_prms(prms):
+    '''Internal. Convert parameter list to user arguments.'''
+    amp = np.array(prms[0::3]); amp.shape = (-1,1)
+    avg = np.array(prms[1::3]); avg.shape = (-1,1)
+    sig = np.array(prms[2::3]); sig.shape = (-1,1)
+    return amp, avg, sig
+
 def _gauss(x, *prms):
-    '''Internal Gaussian model that is fit to the data.'''
-    amp, avg, sig = prms
-    return amp * np.exp(-(x-avg)**2/(2.*sig**2))
+    '''Internal. Gaussian model that is fit to the data.'''
+    amp, avg, sig = _unpack_prms(prms)
+    ans = amp * np.exp(-(x-avg)**2/(2.*sig**2))
+    return np.sum(amp * np.exp(-(x-avg)**2/(2.*sig**2)), axis=0)
 
 def gaussfit(x, y, amp=1., avg=0., sig=1., return_cov=False):
     '''Fit amp, avg, and sig for a Gaussian [y = amp * e^(-(x-avg)^2/(2*sig^2)].
+    amp/avg/sig can be lists/arrays to simultaneously fit multiple Gaussians.
     Parameters
     ----------
     x : x coordinate at which Gaussian is evaluated
     y : measured y coordinate to which Gaussian is compared
-    amp : first guess at amp, the amplitude of the Gaussian, default=1.
-    avg : first guess at avg, the average of the Gaussian, default=0.
-    sig : first guess at sig, the width of the Gaussian, default=1.
+    amp : first guess at amp, the amplitude(s) of the Gaussian(s), default=1.
+    avg : first guess at avg, the average(s) of the Gaussian(s), default=0.
+    sig : first guess at sig, the width(s) of the Gaussian(s), default=1.
     return_cov : return the [amp, avg, sig] covariance matrix of the solution
 
     Returns
     -------
     ans : dictionary with amp/avg/sig keys and fit solutions as values.
     prm_covariance : the [amp, avg, sig] covariance matrix'''
-    prms0 = (amp, avg, sig)
-    (amp,avg,sig), prm_covariance = curve_fit(_gauss, x, y, p0=prms0)
-    ans = {'amp':amp, 'avg':avg, 'sig':sig}
+    prms0 = _pack_prms(amp, avg, sig)
+    prms, prm_covariance = curve_fit(_gauss, x, y, p0=prms0)
+    amp, avg, sig = _unpack_prms(prms)
+    ans = {'amp':amp.flatten(), 'avg':avg.flatten(), 'sig':sig.flatten()}
     if return_cov:
         return ans, prm_covariance
     else:
@@ -31,15 +47,17 @@ def gaussfit(x, y, amp=1., avg=0., sig=1., return_cov=False):
 
 def gaussval(x, amp, avg, sig):
     '''Evaluate a Gaussian given amp, avg, and sig [y = amp * e^(-(x-avg)^2/(2*sig^2)].
+    amp/avg/sig can be lists/arrays to simultaneously fit multiple Gaussians.
     Parameters
     ----------
     x : x coordinate at which Gaussian is evaluated
-    amp : amplitude of Gaussian
-    avg : center point of Gaussian
-    sig : width of Gaussian
+    amp : amplitude(s) of Gaussian(s)
+    avg : center point(s) of Gaussian(s)
+    sig : width(s) of Gaussian(s)
 
     Returns
     -------
     y : the evaluated Gaussian [y = amp * e^(-(x-avg)^2/(2*sig^2)]'''
-    return _gauss(x, amp, avg, sig)
+    prms = _pack_prms(amp, avg, sig)
+    return _gauss(x, *prms)
     
--- ugradio_code/src/tests/test_gauss.py
+++ ugradio_code/src/tests/test_gauss.py
@@ -5,6 +5,24 @@ import numpy as np
 np.random.seed(0)
 
 class TestGauss(unittest.TestCase):
+    def test_pack_prms(self):
+        amp0, avg0, sig0 = 1.5, .1, 2.2
+        prms0 = ugradio.gauss._pack_prms(amp0,avg0,sig0)
+        np.testing.assert_equal(prms0, np.array([amp0, avg0, sig0]))
+        amp0, avg0, sig0 = [1.,2], [3.,4], [5.,6]
+        prms0 = ugradio.gauss._pack_prms(amp0,avg0,sig0)
+        np.testing.assert_equal(prms0, np.array([1.,3,5,2,4,6]))
+    def test_unpack_prms(self):
+        amp0,avg0,sig0 = ugradio.gauss._unpack_prms(np.array([1,2,3]))
+        self.assertEqual(amp0, 1)
+        self.assertEqual(avg0, 2)
+        self.assertEqual(sig0, 3)
+        prms0 = np.array([1.,3,5,2,4,6])
+        amp0, avg0, sig0 = [[1.],[2]], [[3.],[4]], [[5.],[6]]
+        amp, avg, sig = ugradio.gauss._unpack_prms(prms0)
+        np.testing.assert_equal(amp, np.array(amp0))
+        np.testing.assert_equal(avg, np.array(avg0))
+        np.testing.assert_equal(sig, np.array(sig0))
     def test_perfect_gaussian(self):
         amp0, avg0, sig0 = 1.5, .1, 2.2
         x = np.linspace(-6,6, 1024)
@@ -24,6 +42,21 @@ class TestGauss(unittest.TestCase):
         self.assertAlmostEqual(ans['avg'], avg0, 1)
         self.assertAlmostEqual(ans['sig'], sig0, 1)
         np.testing.assert_allclose(y, ugradio.gauss.gaussval(x, **ans), atol=.3)
+    def test_two_gaussian(self):
+        amp0, avg0, sig0 = 1.5, .1, 2.2
+        amp1, avg1, sig1 = 2.5, 1.1, 1.2
+        x = np.linspace(-6,6, 1024)
+        y0 = amp0 * np.exp(-(x-avg0)**2 / (2*sig0**2))
+        y1 = amp1 * np.exp(-(x-avg1)**2 / (2*sig1**2))
+        y = y0 + y1
+        ans = ugradio.gauss.gaussfit(x, y, amp=[1,1], avg=[0,1], sig=[1,1])
+        self.assertAlmostEqual(ans['amp'][0], amp0, 6)
+        self.assertAlmostEqual(ans['avg'][0], avg0, 6)
+        self.assertAlmostEqual(ans['sig'][0], sig0, 6)
+        self.assertAlmostEqual(ans['amp'][1], amp1, 6)
+        self.assertAlmostEqual(ans['avg'][1], avg1, 6)
+        self.assertAlmostEqual(ans['sig'][1], sig1, 6)
+        np.testing.assert_allclose(y, ugradio.gauss.gaussval(x, **ans), 6)
 
 if __name__ == '__main__':
     unittest.main()
