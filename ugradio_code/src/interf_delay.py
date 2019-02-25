c8eb1615c50065d2d5e4e019e5e3a538242931ce
Aaron Parsons <aparsons@berkeley.edu>
2019-02-25 14:26:33 -0800

    Tidied code in interf_delay.py

--- ugradio_code/src/interf_delay.py
+++ ugradio_code/src/interf_delay.py
@@ -5,14 +5,14 @@ simultaneous bandwidth to be used for better sensitivity.'''
 
 from __future__ import print_function
 import socket, thread
+import math
 import time # XXX I think this is unused.
 
 PORT = 1421
 HOST = '10.32.92.121'    # Raspberry Pi connected to delay line control
 MAX_DELAY = 32
-MIN_DELAY = -32
 
-def encode_delay(time_ns):
+def encode_delay(time_ns, dt=0.5, N=8):
     '''Convert a desired delay in nanoseconds into a relay configuration.
 
     Parameters
@@ -22,7 +22,7 @@ def encode_delay(time_ns):
     Returns
     -------
     relay_config : a string of 0's and 1's encoding the relay configuration'''
-    assert(MIN_DELAY <= time_ns <= MAX_DELAY)
+    assert(-MAX_DELAY <= time_ns <= MAX_DELAY)
     # Conversion Explanation:
     # the number of bits are 128 (2 to the power of 7)
     # Range is +-32 nano seconds = 64 nano seconds total
@@ -35,76 +35,11 @@ def encode_delay(time_ns):
     # new 8 bit binary number (8 bits produced by the left shift register function)
     # The 8 bit Xor result will be applied to the individual relay circuits.
 
-    time_delay = time_ns
-
-    # Determin the sign of the delay
-    if  time_delay < 0:
-        sign = False
-    else:
-        sign = True
-
-    if sign == True:
-        delay_total = 32 - time_delay
-    if sign == False:
-        delay_total = abs(time_delay) +32
-    number_of_bits = int(delay_total/0.5)
-    if number_of_bits == 128: number_of_bits = 127
-    binary_value = bin(number_of_bits)
-# Remove the b character embedded with the binary work produced in the bin() function
-    binary_value =  binary_value.replace('b', '0')
-# process binary number to a usable relay drive configuration
-#setregisters
-    R0= '0' ; R1 = '0' ; R2 = '0' ; R3= '0' ; R4 = '0' ; R5 = '0' ; R6 = '0' ; R7 = '0'
-    binary_num_index = (len (binary_value))
-    index = 1
-    if binary_num_index >= index:
-        R0 = binary_value[-1]
-        index = index +1
-    if binary_num_index >= index:
-        R1 = binary_value[-2] 
-        index = index +1
-    if binary_num_index >=   index:
-        R2 = binary_value[-3]  
-        index = index +1
-    if binary_num_index >= index:
-        R3 = binary_value[-4]
-        index = index +1
-    if binary_num_index >=  index:
-        R4 = binary_value[-5]
-        index = index +1
-    if binary_num_index >= index:
-        R5 = binary_value[-6] 
-        index = index +1
-    if binary_num_index >= index:
-        R6 = binary_value[-7] 
-        index = index +1
-# Proform a left shift register function
-    R7_post_shift = R6 ; R6_post_shift = R5 ; R5_post_shift = R4 ; R4_post_shift = R3
-    R3_post_shift = R2 ; R2_post_shift = R1 ;R1_post_shift = R0 
-    R0_post_shift = '0'
-
-# type R0 through R7 and R0_post_shift through R7_post_shift from str to bool
-#This is needed in order for the Xor function to work properly
-    R0=bool(int(R0)) ; R1=bool(int(R1)) ; R2=bool(int(R2)) ; R3=bool(int(R3)) ; R4=bool(int(R4))
-    R5=bool(int(R5)) ; R6=bool(int(R6)) ; R7=bool(int(R7)) 
-
-    R0_post_shift = bool(int(R0_post_shift)) ; R1_post_shift = bool(int(R1_post_shift))
-    R2_post_shift = bool(int(R2_post_shift))  ; R3_post_shift = bool(int(R3_post_shift)) 
-    R4_post_shift = bool(int(R4_post_shift))  ; R5_post_shift = bool(int(R5_post_shift))  
-    R6_post_shift = bool(int(R6_post_shift)) ; R7_post_shift = bool(int(R7_post_shift)) 
-# Perform Xor function
-    k0 = R0 ^ R0_post_shift ; k1 = R1 ^ R1_post_shift ; k2 = R2 ^ R2_post_shift 
-    k3 = R3 ^ R3_post_shift ; k4 = R4 ^ R4_post_shift ; k5 = R5 ^ R5_post_shift  
-    k6 = R6 ^ R6_post_shift ; k7 = R7 ^ R7_post_shift
-#convert k0 through k7 from bool to int
-    k0 = int(k0); k1 = int(k1); k2 = int(k2); k3 = int(k3); k4 = int(k4); 
-    k5 = int(k5); k6 = int(k6); k7 = int(k7)
-#convert k0 through k7 from int to string
-    k0 = str(k0); k1 = str(k1); k2 = str(k2); k3 = str(k3); k4 = str(k4); 
-    k5 = str(k5); k6 = str(k6); k7 = str(k7)
-# concatenate k1 through k7 into a binary represented string word
-    relay_config = k7 + k6 + k5 + k4 + k3 + k2 + k1 + k0
-    resp = self.write_relays(relay_config)
+    delay_total = MAX_DELAY - time_ns
+    dly_cnts = int(round(delay_total / dt))
+    c = min(dly_cnts, 2**(N-1) - 1)
+    c = bin(c ^ (2 * c))[2:] # bitwise xor with left-shifted numbers
+    relay_config = '0' * (N - len(c)) + c # pad out to 8 characters
     return relay_config
 
 class DelayClient:
