ea0698fb903871af6948e0c94f56a582d19e5aa1
Aaron Parsons <aparsons@berkeley.edu>
2024-04-13 14:56:00 -0700

    Got sdr working with new multiple-sdr support.

--- ugradio_code/src/sdr.py
+++ ugradio_code/src/sdr.py
@@ -1,19 +1,13 @@
 """This module uses the pyrtlsdr package (built on librtlsdr) to interface
 to SDR dongles based on the RTL2832/R820T2 chipset."""
 
-from __future__ import print_function
 from rtlsdr import RtlSdr
 import numpy as np
-import logging
-import functools
 import asyncio
-import signal
 import time
 
-BUFFER_SIZE = 4096
-
 async def _streaming(q, sdr, nsamples):
-    '''Asynchronously read nblocks of data from the sdr.'''
+    '''Asynchronously stream samples from an sdr onto a Queue.'''
     async for samples in sdr.stream(num_samples_or_bytes=2*nsamples, format='bytes'):
         t = time.time()
         samples = (np.frombuffer(samples, dtype='uint8') - 128).view('int8')
@@ -23,17 +17,17 @@ async def _streaming(q, sdr, nsamples):
         await q.put((sdr.device_index, t, samples))
 
 async def _collate_streams(q, sdrs, nblocks, nsamples):
+    '''Process queue samples from multiple sdrs into blocks of data collated by sdr.'''
     shape = (nblocks, nsamples) if sdrs[0].direct else (nblocks, nsamples, 2)
     data = {sdr.device_index: np.empty(shape, dtype='int8') for sdr in sdrs}
     cnts = {sdr.device_index: 0 for sdr in sdrs}
     t_start = time.time()
     try:
         while True:
-        #for _ in range(nblocks):
             dev_id, t, samples = await q.get()
-            if t < t_start or cnts[dev_id] > nblocks:
+            if t < t_start or cnts[dev_id] >= nblocks:
                 continue
-            data[cnts[dev_id]] = samples
+            data[dev_id][cnts[dev_id]] = samples
             cnts[dev_id] += 1
             q.task_done()
             if all([cnt >= nblocks for cnt in cnts.values()]):
@@ -43,23 +37,37 @@ async def _collate_streams(q, sdrs, nblocks, nsamples):
             await sdr.stop()  # this closes the async for loop in _streaming
     return data
 
-#def handle_exception(loop, context, sdr):
-#    '''Handle any exceptions that happen while in the asyncio loop.'''
-#    msg = context.get("exception", context["message"])
-#    logging.error(f"Caught exception: {msg}")
-#    if loop.is_running():
-#        asyncio.create_task(shutdown(loop, sdr))
-#
-#async def shutdown(loop, sdr, signal=None):
-#    '''If an interrupt happens, shut down gracefully.'''
-#    if signal:
-#        logging.info(f"Received exit signal {signal.name}...")
-#    if loop.is_running():
-#        tasks = [t for t in asyncio.all_tasks() if t is not 
-#                 asyncio.current_task()]
-#        await sdr.stop()
-#        await asyncio.gather(*tasks, return_exceptions=True)
-#        loop.stop()
+def capture_data(sdrs, nsamples=2048, nblocks=1):
+    """
+    Use SDR dongles to capture voltage samples.
+    Note: the SDR analog system only passes signals from 0.5 to 24 MHz.
+
+    Arguments:
+        sdrs (SDR): an SDR object or a list of SDR objects.
+        nsamples (int): number of samples to acquire. Default: 2048.
+        nblocks (int): number of blocks of samples to acquire. Default:1
+
+    Returns:
+       dict of {device_index: block_of_samples}, where each device_index
+        corresponds to the SDR provided, and each block_of_samples
+        is a numpy.ndarray of type int8 with shape (nblocks, nsamples)
+        (direct == True) or (nblocks, nsamples, 2) (direct == False). 
+    """
+    if isinstance(sdrs, SDR):
+        sdrs = [sdrs]  # wrap a bare sdr object into a list, if provided
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    q = asyncio.Queue()
+    try:
+        producers = [loop.create_task(_streaming(q, sdr, nsamples)) for sdr in sdrs]
+        data = loop.run_until_complete(
+                    _collate_streams(q, sdrs, nblocks, nsamples)
+                )  # closes sdr on exit
+        asyncio.gather(*producers)  # cleanly exit the _streaming loops
+    finally:
+        loop.close()
+    return data
+
 
 class SDR(RtlSdr):
     def __init__(self, device_index=0, direct=True, center_freq=1420e6,
@@ -100,7 +108,6 @@ class SDR(RtlSdr):
         self.set_sample_rate(sample_rate)
         if fir_coeffs is not None:
             self.set_fir_coeffs(fir_coeffs)
-        _ = self.read_samples(BUFFER_SIZE)  # clear the buffer
 
     def __del__(self):
         self.close()
@@ -118,32 +125,5 @@ class SDR(RtlSdr):
            numpy.ndarray of type int8 with shape (nblocks, nsamples)
            (direct == True) or (nblocks, nsamples, 2) (direct == False). 
         """
-        # Make a new event loop and set it as the default
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        q = asyncio.Queue()
-        #stop_event = asyncio.Event()
-        try:
-            ## Add signal handlers
-            #for s in (signal.SIGHUP, signal.SIGTERM, signal.SIGINT):
-            #    loop.add_signal_handler(s,
-            #        lambda: asyncio.create_task(
-            #                    shutdown(loop, self, signal=s)
-            #                )
-            #    )
-            ## splice sdr handle into handle_exception arguments
-            #h = functools.partial(handle_exception, sdr=self)
-            #loop.set_exception_handler(h)
-            producer = loop.create_task(_streaming(q, self, nsamples))
-            data = loop.run_until_complete(
-                        _collate_streams(q, [self], nblocks, nsamples)
-                    )
-            #producer.cancel()
-            #try:
-            asyncio.gather(producer)
-            #except asyncio.CancelledError:
-            #    pass
-        finally:
-            loop.close()
-
+        data = capture_data(self, nsamples=nsamples, nblocks=nblocks)
         return data[self.device_index]
