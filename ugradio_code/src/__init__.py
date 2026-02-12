b811ea7ac97ff2ed2d261a3e1f1df8dbe383b916
Aaron Parsons <aparsons@berkeley.edu>
2026-02-12 15:09:22 -0800

    Changed import ugradio to warn about missing modules but proceed anyway

--- ugradio_code/src/__init__.py
+++ ugradio_code/src/__init__.py
@@ -1,27 +1,34 @@
+import warnings
+
 try:
-    from . import timing # ImportError if astropy not installed
-    from . import doppler # ImportError if astropy not installed
-    from . import coord # ImportError if astropy not installed
+    # These modules will not import if astropy is not installed
+    from . import timing
+    from . import doppler
+    from . import coord
 except ImportError as error:
-    print(error) # Does not pass silently
-    pass
+    #warnings.warn(error)
+    warnings.warn("(Modules timing, doppler, coord unavailable if astropy is not installed)")
+
 try:
-    from . import gauss # ImportError if scipy not installed
+    # These modules will not import if scipy is not installed
+    from . import gauss
 except ImportError as error:
-    print(error) # Does not pass silently
-    pass
+    #warnings.warn(error)
+    warnings.warn("(Module gauss unavailable if scipy is not installed)")
+
 try:
-    from . import sdr # ImportError if rtlsdr not installed
-except (SyntaxError,ImportError) as error:
-    print(error) # Does not pass silently
-    pass
+    # These modules will not import if rtlsdr is not installed
+    from . import sdr
+except (ImportError) as error:
+    #warnings.warn(error)
+    warnings.warn("(Module sdr unavailable if pyrtlsdr is not installed)")
 
 from . import pico
 from . import dft
 from . import agilent
 from . import hp_multi
 from . import interf
-from . import interf_delay
+#from . import interf_delay
 from . import leusch
 from . import nch
 from . import leo
